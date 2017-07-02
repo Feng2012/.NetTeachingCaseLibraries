65cea4d494051064cb77bb2fdb5b5111426771b5
桂素伟 <axzxs2001@163.com>
2017-07-02 23:53:09 +0800

    自动贩卖机完成。

--- DotNetTeachingCaseLibraries/ConsoleVendingMachine/Program.cs
+++ DotNetTeachingCaseLibraries/ConsoleVendingMachine/Program.cs
@@ -9,11 +9,109 @@ namespace ConsoleVendingMachine
     {
         static void Main(string[] args)
         {
+            Start();
         }
         static void Start()
         {
-            var goodses = Restock();
-            ShowGoodses(goodses);
+            while (true)
+            {
+                Console.Clear();
+                //上货
+                var goodses = Restock();
+                //显示商品
+                ShowGoodses(goodses);
+                //处理结果
+                var result = false;
+                //编号
+                var no = 0;
+                //选择商品
+                Goods selectGoods = null;
+                //商品序号输入验证
+                do
+                {
+                    Console.WriteLine("请选择购买的商品编号：");
+                    var noStr = Console.ReadLine();
+                    //偿试转换商品编号，查询是否有效
+                    result = int.TryParse(noStr, out no);
+                    if (!result)
+                    {
+                        Console.WriteLine("请输入有效编号！");
+                        continue;
+                    }
+                    //按编号查询商品
+                    foreach (var goods in goodses)
+                    {
+                        if (goods.No == no)
+                        {
+                            selectGoods = goods;
+                        }
+                    }
+                    //查询是否有此编号商品
+                    if(selectGoods==null)
+                    {
+                        result = false;
+                    }
+                } while (!result);
+               
+                //商品数量输入验证
+                var quantity = 0;
+                do
+                {
+
+                    Console.WriteLine("请选择购买数量：");
+                    var quantityStr = Console.ReadLine();
+                    //偿试转换数量
+                    result = int.TryParse(quantityStr, out quantity);
+                    if (!result)
+                    {
+                        Console.WriteLine("请输入有效数量！");
+                        continue;
+                    }
+                    //判断输入数据是否小于商品数实际数据
+                    if (selectGoods.Quantity < quantity)
+                    {
+                        Console.WriteLine("输入的数据大于现有数据！");
+                        result=false;
+                    }
+
+                } while (!result);
+
+                //应付金额
+                decimal payable = selectGoods.Price * quantity;
+                //已付金额
+                decimal pay = 0;
+                do
+                {
+                    //提示输入金额
+                    Console.WriteLine($"请支付{payable - pay}元：");
+                    var payStr = Console.ReadLine();
+                    var payNow = 0m;
+                    //转换金额
+                    result = decimal.TryParse(payStr, out payNow);
+                    if (!result)
+                    {
+                        Console.WriteLine("请输入有效金额！");
+                        continue;
+                    }
+                    pay += payNow;
+                    //判断应付是否小于等于已付金额
+                    if (pay < payable)
+                    {
+                        result = false;
+                    }
+
+                } while (!result);
+                //找零
+                if(payable<pay)
+                {
+                    Console.WriteLine($"找零{pay-payable}");
+                  
+                }
+                Console.WriteLine("欢迎下次使用！");
+
+                System.Threading.Thread.Sleep(3000);
+
+            }
         }
 
         /// <summary>
@@ -36,10 +134,12 @@ namespace ConsoleVendingMachine
         /// <param name="goodses"></param>
         static void ShowGoodses(List<Goods> goodses)
         {
-            foreach(var goods in goodses)
+            Console.WriteLine("************************************************");
+            foreach (var goods in goodses)
             {
                 Console.WriteLine(goods);
             }
+            Console.WriteLine("************************************************");
         }
     }
 
