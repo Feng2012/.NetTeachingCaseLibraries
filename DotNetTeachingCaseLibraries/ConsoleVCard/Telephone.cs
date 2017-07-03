086da0f90bad310b3e6e27ecfb53d61b2446a31e
桂素伟 <axzxs2001@163.com>
2017-07-03 14:24:31 +0800

    更名片管理项目名称。

--- DotNetTeachingCaseLibraries/ConsoleVCard/Telephone.cs
+++ DotNetTeachingCaseLibraries/ConsoleVCard/Telephone.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
-namespace ConsoleVCard
+namespace ConsoleVCardManagement
 {
     /// <summary>
     /// 固话类
