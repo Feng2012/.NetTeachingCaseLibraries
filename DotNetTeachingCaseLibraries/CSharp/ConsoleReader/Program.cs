4cd7ecbefead1780b6a7fee555dce0cac750c171
桂素伟 <axzxs2001@163.com>
2017-07-07 09:39:04 +0800

    完成ConsoleReader

--- DotNetTeachingCaseLibraries/CSharp/ConsoleReader/Program.cs
+++ DotNetTeachingCaseLibraries/CSharp/ConsoleReader/Program.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using System.Diagnostics;
+
 namespace ConsoleReader
 {
     class Program
@@ -17,7 +19,7 @@ namespace ConsoleReader
         /// </summary>
         static void LoadBooks()
         {
-            var path = "~/books";
+            var path =Directory.GetCurrentDirectory()+@"/books";
             var sn = 1;
             var books = Directory.GetFiles(path);
             //遍历当前路径下的所有图书
@@ -25,20 +27,51 @@ namespace ConsoleReader
             {
                 Console.WriteLine($"{sn}、{Path.GetFileName(bookFile)}");
             }
-            Console.WriteLine("请选择书阅读书籍序号：");
-            var selectSn = Console.ReadLine();
-            var index = 0;
-            if (int.TryParse(selectSn, out index))
+            //选择书籍
+            if (books.Length > 0)
             {
-                var bookFile = books[index - 1];           
-
+                Console.WriteLine("请选择书阅读书籍序号：");
+                var selectSn = Console.ReadLine();
+                var index = 0;
+                if (int.TryParse(selectSn, out index))
+                {
+                    var bookFile = books[index - 1];
+                    ReadBook(bookFile);
+                }
+            }
+            else 
+            {
+                Console.WriteLine("查询不到Books下有书籍！");
             }
         }
-
-        static void LoadBook(string filePath)
+        /// <summary>
+        /// 读书
+        /// </summary>
+        /// <param name="filePath">书的路径</param>
+        static void ReadBook(string filePath)
         {
+            //加载书的内容
             var bookRows = File.ReadAllLines(filePath);
-
+            //设计vbs脚本路径
+            var file = Directory.GetCurrentDirectory() + "/speak.vbs";
+            foreach (var words in bookRows)
+            {
+                //创建 vbs脚本
+                var content = $"CreateObject(\"SAPI.SpVoice\").Speak \"{words}\"";
+                File.WriteAllText(file, content,Encoding.Default);
+                //开始阅读
+                Process.Start(file);
+                Console.WriteLine("---------------------------------------------------");
+                Console.WriteLine(words);
+                Console.WriteLine("---------------------------------------------------");
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("下一段请按任意键，退出请按e");
+                Console.ResetColor();
+                if(Console.ReadKey(true).KeyChar=='e')
+                {
+                    break;
+                }
+            }
         }
     }
 }
