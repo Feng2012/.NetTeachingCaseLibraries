8e32942dd505162bb977ccb18a1b30e2d81d9af1
桂素伟 <axzxs2001@163.com>
2017-07-09 00:16:18 +0800

    分布式日志，基于RabbitMQ

---
+++ DotNetTeachingCaseLibraries/DistributedLogWriter/Program.cs
@@ -0,0 +1,44 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace DistributedLogWriter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    //创建一个名为"hello"的队列，防止producer端没有创建该队列
+                    channel.QueueDeclare(queue: "hello",
+           durable: false,
+           exclusive: false,
+           autoDelete: false,
+           arguments: null);
+
+                    //回调，当consumer收到消息后会执行该函数
+                    var consumer = new EventingBasicConsumer(channel);
+                    consumer.Received += (model, ea) =>
+                    {
+                        var body = ea.Body;
+                        var message = Encoding.UTF8.GetString(body);
+                        Console.WriteLine(" [x] Received {0}", message);
+                    };
+
+                    //消费队列"hello"中的消息
+                    channel.BasicConsume(queue: "hello",
+           noAck: true,
+           consumer: consumer);
+
+                    Console.WriteLine(" Press [enter] to exit.");
+                    Console.ReadLine();
+                }
+            }
+        }
+    }
+}
---
+++ DotNetTeachingCaseLibraries/Web/asp.net core/DistributedLog/Controllers/HomeController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using RabbitMQ.Client;
+using System.Text;
+
+namespace DistributedLog.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet("writelog")]
+        public IActionResult Write(string sendMessage)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    //创建一个名叫"hello"的消息队列
+                    channel.QueueDeclare(queue: "hello",
+       durable: false,
+       exclusive: false,
+       autoDelete: false,
+       arguments: null);
+
+                    var message = sendMessage;
+                    var body = Encoding.UTF8.GetBytes(message);
+
+                    //向该消息队列发送消息message
+                    channel.BasicPublish(exchange: "",
+       routingKey: "hello",
+       basicProperties: null,
+       body: body);
+                    Console.WriteLine(" [x] Sent {0}", message);
+                }
+
+            }
+            return new JsonResult(true);
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
