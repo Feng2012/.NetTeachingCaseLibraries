2f1d13219245969b5bcb0546d27f911f1cd5aa1f
桂素伟 <axzxs2001@163.com>
2017-07-24 14:11:47 +0800

    单元测试处理异常测试。

--- DotNetTeachingCaseLibraries/CSharp/NUnite/CalculatorManagement/Calculator.cs
+++ DotNetTeachingCaseLibraries/CSharp/NUnite/CalculatorManagement/Calculator.cs
@@ -46,5 +46,46 @@ namespace CalculatorManagement
                 return result;
             }
         }
+        /// <summary>
+        /// 乘法运算
+        /// </summary>
+        /// <param name="parameters">参数</param>
+        /// <returns></returns>
+        public double Multiply(params double[] parameters)
+        {
+            var result = 0d;
+            foreach (var parameter in parameters)
+            {
+                result *= parameter;
+            }
+            return result;
+        }
+        /// <summary>
+        /// 除法运算
+        /// </summary>
+        /// <param name="parameters
+        public double Divide(params double[] parameters)
+        {
+            if (parameters.Length == 0)
+            {
+                return 0;
+            }
+            else
+            {
+                var result = parameters[0];
+                for (var i = 1; i < parameters.Length; i++)
+                {
+                    if (parameters[i] == 0)
+                    {
+                        throw new DivideByZeroException($"第{i + 1}个参数不能为零");
+                    }
+                    else
+                    {
+                        result /= parameters[i];
+                    }
+                }
+                return result;
+            }
+        }
     }
 }
