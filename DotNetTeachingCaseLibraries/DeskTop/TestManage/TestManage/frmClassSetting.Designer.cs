a581f5c3cf708f34a828b682fff082e9dd019758
桂素伟 <axzxs2001@163.com>
2017-08-06 09:37:10 +0800

    班级窗体设计。

--- DotNetTeachingCaseLibraries/DeskTop/TestManage/TestManage/frmClassSetting.Designer.cs
+++ DotNetTeachingCaseLibraries/DeskTop/TestManage/TestManage/frmClassSetting.Designer.cs
@@ -28,19 +28,156 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmClassSetting));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.labClassName = new System.Windows.Forms.Label();
+            this.txbClassName = new System.Windows.Forms.TextBox();
+            this.txbMemo = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.ilistOpt = new System.Windows.Forms.ImageList(this.components);
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnDelete);
+            this.panel1.Controls.Add(this.btnEdit);
+            this.panel1.Controls.Add(this.btnAdd);
+            this.panel1.Controls.Add(this.txbMemo);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.txbClassName);
+            this.panel1.Controls.Add(this.labClassName);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(390, 120);
+            this.panel1.TabIndex = 0;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 120);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.Size = new System.Drawing.Size(390, 261);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // labClassName
+            // 
+            this.labClassName.AutoSize = true;
+            this.labClassName.Location = new System.Drawing.Point(12, 10);
+            this.labClassName.Name = "labClassName";
+            this.labClassName.Size = new System.Drawing.Size(65, 12);
+            this.labClassName.TabIndex = 0;
+            this.labClassName.Text = "班级名称：";
+            // 
+            // txbClassName
+            // 
+            this.txbClassName.Location = new System.Drawing.Point(76, 6);
+            this.txbClassName.Name = "txbClassName";
+            this.txbClassName.Size = new System.Drawing.Size(302, 21);
+            this.txbClassName.TabIndex = 1;
+            // 
+            // txbMemo
+            // 
+            this.txbMemo.Location = new System.Drawing.Point(76, 32);
+            this.txbMemo.Multiline = true;
+            this.txbMemo.Name = "txbMemo";
+            this.txbMemo.Size = new System.Drawing.Size(304, 50);
+            this.txbMemo.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "班级备注：";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.ImageIndex = 0;
+            this.btnAdd.ImageList = this.ilistOpt;
+            this.btnAdd.Location = new System.Drawing.Point(176, 85);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(65, 31);
+            this.btnAdd.TabIndex = 4;
+            this.btnAdd.Text = "添加";
+            this.btnAdd.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnAdd.UseVisualStyleBackColor = true;
+            // 
+            // ilistOpt
+            // 
+            this.ilistOpt.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("ilistOpt.ImageStream")));
+            this.ilistOpt.TransparentColor = System.Drawing.Color.Transparent;
+            this.ilistOpt.Images.SetKeyName(0, "Add.png");
+            this.ilistOpt.Images.SetKeyName(1, "Del.png");
+            this.ilistOpt.Images.SetKeyName(2, "Edit.png");
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.ImageIndex = 2;
+            this.btnEdit.ImageList = this.ilistOpt;
+            this.btnEdit.Location = new System.Drawing.Point(245, 85);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(65, 31);
+            this.btnEdit.TabIndex = 5;
+            this.btnEdit.Text = "修改";
+            this.btnEdit.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnEdit.UseVisualStyleBackColor = true;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.ImageIndex = 0;
+            this.btnDelete.ImageList = this.ilistOpt;
+            this.btnDelete.Location = new System.Drawing.Point(314, 85);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(65, 31);
+            this.btnDelete.TabIndex = 6;
+            this.btnDelete.Text = "删除";
+            this.btnDelete.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnDelete.UseVisualStyleBackColor = true;
             // 
             // frmClassSetting
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(533, 470);
+            this.ClientSize = new System.Drawing.Size(390, 381);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "frmClassSetting";
-            this.Text = "frmClassSetting";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "班级管理";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.TextBox txbMemo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txbClassName;
+        private System.Windows.Forms.Label labClassName;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.ImageList ilistOpt;
+        private System.Windows.Forms.Button btnEdit;
     }
 }
