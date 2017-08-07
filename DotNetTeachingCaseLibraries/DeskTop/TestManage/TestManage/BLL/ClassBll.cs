1074aca5ded2d65984ac63cd08d948e9e321b811
桂素伟 <axzxs2001@163.com>
2017-08-07 20:33:47 +0800

    创建班级业务操作类。

---
+++ DotNetTeachingCaseLibraries/DeskTop/TestManage/TestManage/BLL/ClassBll.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestManage.DDL;
+
+namespace TestManage.BLL
+{
+    public class ClassBll
+    {
+        TestManageModel _testManageDB;
+        public ClassBll(TestManageModel testManageDB)
+        {
+            _testManageDB = testManageDB;
+        }
+
+        public bool AddClass(Class cls)
+        {
+            _testManageDB.Classes.Add(cls);
+            var result = _testManageDB.SaveChanges();
+            return result > 0;
+        }
+
+
+    }
+}
