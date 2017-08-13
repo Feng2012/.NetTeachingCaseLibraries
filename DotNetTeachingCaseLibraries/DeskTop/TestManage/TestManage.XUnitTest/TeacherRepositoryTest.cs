977f442d22efcf311baa8cac7b4e861c71987d0a
桂素伟 <axzxs2001@163.com>
2017-08-14 07:59:52 +0800

    班级测试构建完成。

--- DotNetTeachingCaseLibraries/DeskTop/TestManage/TestManage.XUnitTest/TeacherRepositoryTest.cs
+++ DotNetTeachingCaseLibraries/DeskTop/TestManage/TestManage.XUnitTest/TeacherRepositoryTest.cs
@@ -63,9 +63,98 @@ namespace TestManage.XUnitTest
                 .SetupData(data);
             _dbMock.Setup(db => db.Teachers).Returns(teacherSet.Object);
             var backTeacher = _teacherRepository.Login("0001", "222222");
-            Assert.Same(null, backTeacher);
+            Assert.Equal(null, backTeacher);
         }
 
+
+        /// <summary>
+        /// 查询教师测试
+        /// </summary>
+        [Fact]
+        public void GetClasses_Default_ReturnCount()
+        {
+            var data = new List<Teacher> { new Teacher(), new Teacher()};
+            var teacherSet = new Mock<DbSet<Teacher>>()
+                .SetupData(data);
+            _dbMock.Setup(db => db.Teachers).Returns(teacherSet.Object);
+            var list = _teacherRepository.GetTeachers();
+            Assert.Equal(2, list.Count);
+        }
+
+        /// <summary>
+        /// AddTeacher异常测试
+        /// </summary>
+        [Fact]
+        public void AddTeacher_ThrowException_Catch()
+        {
+            var excetionMessage = "AddTeacher异常";
+            _dbMock.Setup(db => db.Teachers.Add(null)).Throws(new Exception(excetionMessage));
+            var ext = Assert.Throws<Exception>(() => _teacherRepository.AddTeacher(null));
+            Assert.Contains(excetionMessage, ext.Message);
+        }
+        /// <summary>
+        /// AddTeacher正常添加
+        /// </summary>
+        [Theory]
+        [InlineData(0)]
+        [InlineData(1)]
+        public void AddTeacher_Default_ReturnTrue(int result)
+        {
+            _dbMock.Setup(db => db.Teachers.Add(new Teacher()));
+            _dbMock.Setup(db => db.SaveChanges()).Returns(value: result);
+            var backResult = _teacherRepository.AddTeacher(new Teacher());
+            Assert.Equal(result == 1, backResult);
+        }
+        /// <summary>
+        /// ModifyTeacher异常测试
+        /// </summary>
+        [Fact]
+        public void ModifyTeacher_ThrowException_Catch()
+        {
+            _dbMock.Setup(db => db.Teachers.Find()).Returns(value: null);
+            var ext = Assert.Throws<Exception>(() => _teacherRepository.ModifyTeacher(new Teacher { ID = 111 }));
+            Assert.Contains("查询不到ID为111的教师", ext.Message);
+        }
+        /// <summary>
+        /// ModifyTeacher正常测试
+        /// </summary>
+        [Theory]
+        [InlineData(0)]
+        [InlineData(1)]
+        public void ModifyTeacher_Default_ReturnTrue(int result)
+        {
+            var teacher = new Teacher { ID = 111 };
+            _dbMock.Setup(db => db.Teachers.Find(teacher.ID)).Returns(value: new Teacher());
+            _dbMock.Setup(db => db.SaveChanges()).Returns(value: result);
+            var backResult = _teacherRepository.ModifyTeacher(teacher);
+            Assert.Equal(result == 1, backResult);
+        }
+
+        /// <summary>
+        /// RemoveTeacher异常测试
+        /// </summary>
+        [Fact]
+        public void RemoveTeacher_ThrowException_Catch()
+        {
+            _dbMock.Setup(db => db.Teachers.Find()).Returns(value: null);
+            var ext = Assert.Throws<Exception>(() => _teacherRepository.RemoveTeacher(111));
+            Assert.Contains("查询不到ID为111的教师", ext.Message);
+        }
+
+        /// <summary>
+        /// RemoveTeacher正常
+        /// </summary>
+        [Theory]
+        [InlineData(1)]
+        [InlineData(0)]
+        public void RemoveTeacher_Default_ReturnClass(int result)
+        {
+            var teacher = new Teacher { ID = 111 };
+            _dbMock.Setup(db => db.Teachers.Find(teacher.ID)).Returns(value: new Teacher());
+            _dbMock.Setup(db => db.SaveChanges()).Returns(value: result);
+            var backResult = _teacherRepository.RemoveTeacher(111);
+            Assert.Equal(result == 1, backResult);
+        }
     }
 
 
