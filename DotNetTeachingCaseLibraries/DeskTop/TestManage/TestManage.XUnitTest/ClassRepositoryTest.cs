0118fc9bcad7db6c35108b1d590e78d63ad3fa06
桂素伟 <axzxs2001@163.com>
2017-08-14 06:54:03 +0800

    解决SingleOrDefault问题。

--- DotNetTeachingCaseLibraries/DeskTop/TestManage/TestManage.XUnitTest/ClassRepositoryTest.cs
+++ DotNetTeachingCaseLibraries/DeskTop/TestManage/TestManage.XUnitTest/ClassRepositoryTest.cs
@@ -88,30 +88,16 @@ namespace TestManage.XUnitTest
         [Fact]
         public void RemoveClass_ThrowException_Catch()
         {
-
-            _dbMock.SetupProperty(db => db.Classes);
-            _dbMock.Setup(db => db.Classes.SingleOrDefault(It.IsAny<Expression<Func<Class, bool>>>())).Returns(value: new Class());
-
-            var ext = Assert.Throws<Exception>(() => _classRepository.RemoveClass(111));
-            Assert.Contains("查询不到ID为111的班级", ext.Message);
-
-
-
+            var data = new List<Class> { new Class { ID = 1, ClassName = "BBB" } };
+            var clsSet = new Mock<DbSet<Class>>()
+                .SetupData(data);
+            _dbMock.Setup(db => db.Classes).Returns(clsSet.Object);
+            var exc = Assert.Throws<Exception>(() => _classRepository.RemoveClass(111));
+            Assert.Contains("查询不到ID为111的班级", exc.Message);
         }
+
     }
 
-    public static class SecurityExtensions
-    {
-        public static Mock<DbSet<T>> AsDbSetMock<T>(this IEnumerable<T> list) where T : class
-        {
-            IQueryable<T> queryableList = list.AsQueryable();
-            Mock<DbSet<T>> dbSetMock = new Mock<DbSet<T>>();
-            dbSetMock.As<IQueryable<T>>().Setup(x => x.Provider).Returns(queryableList.Provider);
-            dbSetMock.As<IQueryable<T>>().Setup(x => x.Expression).Returns(queryableList.Expression);
-            dbSetMock.As<IQueryable<T>>().Setup(x => x.ElementType).Returns(queryableList.ElementType);
-            dbSetMock.As<IQueryable<T>>().Setup(x => x.GetEnumerator()).Returns(() => queryableList.GetEnumerator());
-            return dbSetMock;
-        }
-    }
+
 
 }
