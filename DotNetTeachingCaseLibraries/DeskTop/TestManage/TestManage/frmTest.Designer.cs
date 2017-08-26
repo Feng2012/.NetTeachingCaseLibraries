833a8d75d0b3d33b3c5e81a23ebebd12d8ffd4ee
桂素伟 <axzxs2001@163.com>
2017-08-26 20:01:33 +0800

    考试界面布局。

--- DotNetTeachingCaseLibraries/DeskTop/TestManage/TestManage/frmTest.Designer.cs
+++ DotNetTeachingCaseLibraries/DeskTop/TestManage/TestManage/frmTest.Designer.cs
@@ -28,23 +28,184 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.labTestName = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.labClassName = new System.Windows.Forms.Label();
+            this.labStudentName = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.labQuestionName = new System.Windows.Forms.Label();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel5.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // labTestName
+            // 
+            this.labTestName.Dock = System.Windows.Forms.DockStyle.Top;
+            this.labTestName.Font = new System.Drawing.Font("宋体", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.labTestName.Location = new System.Drawing.Point(10, 10);
+            this.labTestName.Name = "labTestName";
+            this.labTestName.Size = new System.Drawing.Size(928, 22);
+            this.labTestName.TabIndex = 0;
+            this.labTestName.Text = "试卷名称";
+            this.labTestName.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Controls.Add(this.labTestName);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Padding = new System.Windows.Forms.Padding(10);
+            this.panel1.Size = new System.Drawing.Size(948, 77);
+            this.panel1.TabIndex = 1;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.labStudentName);
+            this.panel2.Controls.Add(this.labClassName);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(10, 32);
+            this.panel2.Name = "panel2";
+            this.panel2.Padding = new System.Windows.Forms.Padding(10);
+            this.panel2.Size = new System.Drawing.Size(928, 40);
+            this.panel2.TabIndex = 1;
+            // 
+            // labClassName
+            // 
+            this.labClassName.Dock = System.Windows.Forms.DockStyle.Left;
+            this.labClassName.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.labClassName.Location = new System.Drawing.Point(10, 10);
+            this.labClassName.Name = "labClassName";
+            this.labClassName.Size = new System.Drawing.Size(437, 20);
+            this.labClassName.TabIndex = 1;
+            this.labClassName.Text = "试卷名称";
+            this.labClassName.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // labStudentName
+            // 
+            this.labStudentName.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.labStudentName.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.labStudentName.Location = new System.Drawing.Point(447, 10);
+            this.labStudentName.Name = "labStudentName";
+            this.labStudentName.Size = new System.Drawing.Size(471, 20);
+            this.labStudentName.TabIndex = 2;
+            this.labStudentName.Text = "试卷名称";
+            this.labStudentName.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.panel4);
+            this.panel3.Controls.Add(this.panel5);
+            this.panel3.Controls.Add(this.labQuestionName);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel3.Location = new System.Drawing.Point(0, 77);
+            this.panel3.Name = "panel3";
+            this.panel3.Padding = new System.Windows.Forms.Padding(10);
+            this.panel3.Size = new System.Drawing.Size(948, 573);
+            this.panel3.TabIndex = 2;
+            // 
+            // labQuestionName
+            // 
+            this.labQuestionName.Dock = System.Windows.Forms.DockStyle.Top;
+            this.labQuestionName.Font = new System.Drawing.Font("宋体", 12F);
+            this.labQuestionName.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.labQuestionName.Location = new System.Drawing.Point(10, 10);
+            this.labQuestionName.Name = "labQuestionName";
+            this.labQuestionName.Size = new System.Drawing.Size(928, 216);
+            this.labQuestionName.TabIndex = 1;
+            this.labQuestionName.Text = "试卷名称";
+            // 
+            // panel4
+            // 
+            this.panel4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel4.Location = new System.Drawing.Point(10, 226);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(928, 278);
+            this.panel4.TabIndex = 2;
+            // 
+            // panel5
+            // 
+            this.panel5.Controls.Add(this.label1);
+            this.panel5.Controls.Add(this.button2);
+            this.panel5.Controls.Add(this.button1);
+            this.panel5.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel5.Location = new System.Drawing.Point(10, 504);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(928, 59);
+            this.panel5.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.button1.Location = new System.Drawing.Point(0, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(248, 59);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Dock = System.Windows.Forms.DockStyle.Right;
+            this.button2.Location = new System.Drawing.Point(680, 0);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(248, 59);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Location = new System.Drawing.Point(248, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(432, 59);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
             // 
             // frmTest
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(948, 650);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel1);
             this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "frmTest";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "考试";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.Load += new System.EventHandler(this.frmTest_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.panel5.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label labTestName;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label labStudentName;
+        private System.Windows.Forms.Label labClassName;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label labQuestionName;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
     }
 }
