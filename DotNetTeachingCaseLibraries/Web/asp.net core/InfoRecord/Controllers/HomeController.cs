944a75422174f4868c0d5fc55a502476c8dc440e
桂素伟 <axzxs2001@163.com>
2018-06-07 19:09:03 +0900

    表单提交程序。

---
+++ DotNetTeachingCaseLibraries/Web/asp.net core/InfoRecord/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace InfoRecord.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(Record record)
+        {
+            ViewBag.Message = "<span style='color:red'>ok</span>";
+            return View();
+        }
+
+
+    }
+
+    public class Record
+    {
+        public string CompanyName
+        { get; set; }
+        public string CompanyType
+        { get; set; }
+
+        public string Principal
+        { get; set; }
+        public string Address
+        { get; set; }
+        public string Tel { get; set; }
+
+        public string Conferee { get; set; }
+
+        public string Attendance { get; set; }
+    }
+}
